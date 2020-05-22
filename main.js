85bf2da5d866ad1f83305bfcf98a694460d7b141
glizzwald <4barrk68@solent.ac.uk>
2020-05-22 18:10:14 +0100

    statistics

--- main.js
+++ main.js
@@ -2,15 +2,19 @@ var covid;
 
 function main() {
     var settings = {
-        "url": "https://api.covid19api.com/",
+        "url": "https://api.covid19api.com/summary",
         "method": "GET",
         "timeout": 0,
     };
 
+
+
     $.ajax(settings).done(function(response) {
         console.log(response);
+        document.write(NewConfirmed)
     });
 }
+document.write(NewConfimed)
 
 var output = document.getElementById('output');
 output.innerHTML = 'new content';
